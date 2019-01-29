629ba13e1dd9fe33ad06969dcc2012f8baa9963e
Cameron Freshwater <ccfreshwater@gmail.com>
2019-01-29 15:55:03 -0800

    adjust default quantiles to 90 not 80% interval

--- R/calcQuantile.R
+++ R/calcQuantile.R
@@ -1,16 +1,16 @@
 #' Calculate lower quantile
 #'
 #' Two simple wrapper functions for \code{quantile()} that are used in plotting
-#' functions. qLow defaults to the 10th percentile and qHigh to the 90th.
+#' functions. qLow defaults to the 5th percentile and qHigh to the 95th.
 #'
 #' @param x A numeric vector.
-#' @return Returns a numeric representing either the 10th or 90th percentile.
+#' @return Returns a numeric representing either the 5th or 95th percentile.
 #'
 #' @export
 
 qLow <- function(x) {
-q <- quantile(x, probs = 0.10, na.rm = TRUE)
-return(q)
+  q <- quantile(x, probs = 0.05, na.rm = TRUE)
+  return(q)
 }
 
 #______________________________________________________________________________
@@ -18,14 +18,14 @@ return(q)
 #' Calculate upper quantile
 #'
 #' Two simple wrapper functions for \code{quantile()} that are used in plotting
-#' functions. qLow defaults to the 10th percentile and qHigh to the 90th.
+#' functions. qLow defaults to the 5th percentile and qHigh to the 95th.
 #'
 #' @param x A numeric vector.
-#' @return Returns a numeric representing either the 10th or 90th percentile.
+#' @return Returns a numeric representing either the 5th or 95th percentile.
 #'
 #' @export
 
 qHigh <- function(x) {
-  q <- quantile(x, probs = 0.90, na.rm = TRUE)
+  q <- quantile(x, probs = 0.95, na.rm = TRUE)
   return(q)
 }
