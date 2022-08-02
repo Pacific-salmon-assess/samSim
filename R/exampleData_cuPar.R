e293adadaf8291b50bebe0b7f71a33913dd9e0a1
Catarina Wor <Catarina Wor>
2022-08-02 14:49:25 -0700

    add help cuPAR

---
+++ R/exampleData_cuPar.R
@@ -0,0 +1,52 @@
+#' Example of cuPar input datafile
+#' 
+#' 
+#'
+#' @format A dataframe with 45 columns used as input to define simulation scenarios and MP:
+#' \describe{
+#'   \item{manUnit}{} 
+#'   \item{stkName}{Name of stock}
+#'   \item{model}{stock recruitment model to be used. Options are "ricker", "rickerSurv" and "larkin"}
+#'   \item{domCycle}{}
+#'   \item{minER}{}
+#'   \item{cvER}{} 
+#'   \item{usERScalar}{}
+#'   \item{canERScalar}{} 
+#'   \item{tauCycAge}{}
+#'   \item{alpha}{}
+#'   \item{beta0}{}
+#'   \item{sigma}{}	
+#'   \item{coef1}{}	
+#'   \item{covarInit}{}	
+#'   \item{Sinit}{ Numbers of spawners to initialize simulations, if absent Seq is used}
+#'   \item{mu_logCovar1}{}	
+#'   \item{sig_logCovar1}{}	
+#'   \item{min_logCovar}{}	
+#'   \item{max_logCovar}{}	
+#'   \item{aveGenTime}{}	
+#'   \item{ageFirstRec}{}	
+#'   \item{ageMaxRec}{}	
+#'   \item{larkAlpha}{}	
+#'   \item{larkBeta0}{}	
+#'   \item{larkBeta1}{}	
+#'   \item{larkBeta2}{}	
+#'   \item{larkBeta3}{}	
+#'   \item{larkSigma}{}	
+#'   \item{meanRec2}{}	
+#'   \item{meanRec3}{}	
+#'   \item{meanRec4}{}	
+#'   \item{meanRec5}{}	
+#'   \item{meanRec6}{}	
+#'   \item{medianRec}{}	
+#'   \item{lowQRec}{}	
+#'   \item{highQRec}{}	
+#'   \item{meanDBE}{}	
+#'   \item{sdDBE}{}	
+#'   \item{medMA}{}	
+#'   \item{meanForecast}{}	
+#'   \item{sdForecast}{}
+#'  
+#' }
+"cuParexample"
+
+
